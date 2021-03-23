9e687be97ea17ddc40e40b5a00073e7422fd3151
Lucas Piazzi <lpiazzi26@gmail.com>
2021-03-23 10:45:23 -0300

    Adiciona atributos na tabela de usuários

---
+++ database/migrations/2021_03_23_134041_alter_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('cpf', 15);
+            $table->date('data_de_nascimento');
+            $table->float('salario');
+            $table->string('endereco', 150);
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
